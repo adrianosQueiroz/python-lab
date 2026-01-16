19cb64079ac27807d603896a552455e32d05fb7c
Adriano <adriano.morrison@gmail.com>
2026-01-16 17:17:28 -0300

    Ajuste no exercício 02

--- exercícios/exercicio02.py
+++ exercícios/exercicio02.py
@@ -1,14 +1,21 @@
 import pandas as pd
 
-def analise_estoque_min (data:str)-> pd.DataFrame:
-    
-    estoque_minimo = 950
+def analise_estoque_min (data:str, estoque_minimo: int = 950)-> pd.DataFrame:
+
+    """
+    Função para verificar se produtos listados estão com estoque mínimo. 
+    """
+
     df = pd.read_csv(data)
     produtos_estoque_minimo = df[df['Estoque'] < estoque_minimo]
     return produtos_estoque_minimo
 
 if __name__=="__main__":
-    dados = input("Informe o path do arquivo: ")
+    dados = input("Informe o path do arquivo: ").strip().replace('"','').replace("'",'')
     resultado = analise_estoque_min(dados)
-    print(resultado)
+    if not resultado.empty:
+        print("\n--- Produtos com ruptura no estoque ---")    
+        print(resultado)
+    else:
+        print("\nNenhum produto com ruptura")    
 
